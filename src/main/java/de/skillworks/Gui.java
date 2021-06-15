41fecef7bcf7bb3aa7bbbdff2be91eb9e9284367
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-06-15 14:28:04 +0200

    initial commit

---
+++ src/main/java/de/skillworks/Gui.java
@@ -0,0 +1,16 @@
+package de.skillworks;
+
+import javax.swing.*;
+
+public class Gui {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+
+}
